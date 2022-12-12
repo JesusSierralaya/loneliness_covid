7dd4467db80649afd255efa92fa0207be2a9fe48
Jesus <jesus.sierralaya@hotmail.com>
2022-12-13 00:26:27 +0100

    practice in the creation of the forestplot + table

---
+++ notebooks/multi_table_forest_plot_test.R
@@ -0,0 +1,49 @@
+# library(ggplot2)
+library(gridExtra)
+
+library(tidyverse)
+
+# data for plot
+
+dat <- data.frame(group = factor(c("A","B","C","D","E","F","G"), 
+                                 levels=c("F","E","D","C","B","A","G")),
+                  cen = c(3.1,2.0,1.6,3.2,3.6,7.6,NA),
+                  low = c(2,0.9,0.8,1.5,2,4.2,NA),
+                  high = c(6,4,2,6,5,14.5,NA))
+
+# Theme then
+
+# plot from dat
+
+p <- ggplot(dat,aes(cen,group)) + 
+  geom_point(size=5, shape=18) +
+  geom_errorbarh(aes(xmax = high, xmin = low), height = 0.15) +
+  geom_vline(xintercept = 1, linetype = "longdash") +
+  scale_x_continuous(breaks = seq(0,14,1), labels = seq(0,14,1)) +
+  labs(x="Adjusted Odds Ratio", y="")
+
+# label
+
+lab <- data.frame(V0 = factor(c("A","B","C","D","E","F","G","A","B","C","D","E","F","G","A","B","C","D","E","F","G","A","B","C","D","E","F","G"),levels=c("G","F","E","D","C","B","A")),
+                  V05 = rep(c(1,2,3,4),each=7),
+                  V1 = c("Occuption","Active","","Inactive","","Inactive","","Recreation","Inactive","","Active","","Inactive","","Gender","Men","Women","Men","Women","Men","Women","OR",3.1,2.0,1.6,3.2,3.6,7.6)
+)
+
+# data table
+
+
+data_table <- ggplot(lab, aes(x = V05, y = V0, label = format(V1, nsmall = 1))) +
+  geom_text(size = 4, hjust=0, vjust=0.5) + theme_bw() +
+  # geom_hline(aes(yintercept=c(6.5,7.5))) + 
+  theme(panel.grid.major = element_blank(), 
+        legend.position = "none",
+        panel.border = element_blank(), 
+        axis.text.x = element_text(colour="white"),#element_blank(),
+        axis.text.y = element_blank(), 
+        axis.ticks = element_line(colour="white"),#element_blank(),
+        plot.margin = unit(c(0,0,0,0), "lines")) +
+  labs(x="",y="") +
+  coord_cartesian(xlim=c(1,4.5))
+
+grid.arrange(data_table, p, ncol=2)
+
