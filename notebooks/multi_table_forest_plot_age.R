317a2cd8a901e8b7449ca359366a3929ceb8653d
Jesus <jesus.sierralaya@hotmail.com>
2022-12-14 00:22:36 +0100

    Test data with age grouped part 2

--- notebooks/multi_table_forest_plot_age.R
+++ notebooks/multi_table_forest_plot_age.R
@@ -1,3 +1,6 @@
+library(tidyverse)
+library(gridExtra)
+
 # First test with own data
 
 ## call data
@@ -16,21 +19,21 @@ source("throughput/import_clean.R", encoding = 'UTF-8')
 
 source("throughput/multivariate.R", encoding = 'UTF-8')
 
-table_multi <- tbl_multi$table_body 
+# Data base with all data
 
-# Variable age ------------
+table_multi <- tbl_multi$table_body
 
-## First row: names col
+# TABLE --------------------------
 
-# table_multi |> 
-#   filter(variable == "age") |> 
-#   select(var_label, label, estimate)
+## First row: names col -------------------
 
-names_cols <- c("Variable", "Level", "Beta")
+names_cols <- c("Variable", "Level", "Beta")# editable
 
 num_row <- length(names_cols)
 
 row_letter <- LETTERS[1] |> rep(num_row)
+
+# data base first row
 
 data_table <- tibble(
   rows_table = row_letter,
@@ -38,25 +41,124 @@ data_table <- tibble(
   names_cols
 );data_table
 
-## Var age
+## Variable age ------------
+
+
+# Data base only for ages
 
 data_age <- table_multi |>
-  filter(variable == "age") |>
-  select(var_label, label, estimate)
+  filter(variable == "age") |> # editable
+  select(label, estimate, conf.low, conf.high)
+
+# n categories
+
+n_cat <- nrow(data_age)
 
 # Var label
 
-label_var <- data_age |> select(label) |> filter(row_number()==1) |> pull()
+label_var <- data_age |> 
+  select(label) |> filter(row_number()==1) |> pull()
 
-label_val <- data_age |> select(label) |> filter(row_number()==3) |> pull()
+label_var <- c(label_var, "", "")
 
-label_beta <- data_age |> select(estimate) |> filter(row_number()==3) |> pull() |> round(2)
+# val label
+
+# label_val <- data_age |> 
+#   select(label) |> filter(row_number()==n_cat-2:n_cat) |> pull()
+
+label_val <- 
+  data_age |> 
+  select(label) |>
+  # filter(label %in% 3:4) |> pull()
+  slice((n_cat-2):n_cat)  |> pull() 
 
 
-data_table |> add_row(
-  rows_table = LETTERS[2],
-  cols_table = 1:num_row,
-  names_cols = c(label_var, label_val, label_beta)
+# estimate
+
+# label_beta <- data_age |> 
+#   select(estimate) |> filter(row_number()==n_cat) |> pull() |> round(2)
+
+label_beta <- 
+  data_age |> 
+  select(estimate) |>
+  # filter(label %in% 3:4) |> pull()
+  slice((n_cat-2):n_cat)  |> pull() |> round(2)
+
+# merge row by row B
+
+data_table <- data_table |> add_row(
+  rows_table = LETTERS[2],# B B B
+  cols_table = 1:num_row, # 1 2 3
+  names_cols = c(label_var[1], label_val[1], label_beta[1]) # "Age grouped"             "18-34" "0.133918477829876" 
+)
+
+# merge row by row C
+data_table <- data_table |> add_row(
+  rows_table = LETTERS[3],# B B B
+  coltibbles_table = 1:num_row, # 1 2 3
+  names_cols = c(label_var[2], label_val[2], label_beta[2]) # "Age grouped"             "18-34" "0.133918477829876" 
+)
+
+# merge row by row D
+data_table <- data_table |> add_row(
+  rows_table = LETTERS[4],# B B B
+  cols_table = 1:num_row, # 1 2 3
+  names_cols = c(label_var[3], label_val[3], label_beta[3]) # "Age grouped"             "18-34" "0.133918477829876" 
+)
+
+# arrange like the example
+lab <- data_table |> arrange(cols_table)
+
+# order 
+
+lab <- lab |> mutate(
+  rows_table = rows_table |>
+    ordered(sort(LETTERS[1:4], decreasing = TRUE))
+)
+
+# FOREST PLOT
+
+# dat <- data_age |> select(-label) |> slice((n_cat-2):n_cat) |> 
+#   transmute(
+#     group = LETTERS[1:(n_cat-2)] |>
+#       ordered(sort(LETTERS[1:4], decreasing = TRUE)),
+#     cen = c(NA, estimate),
+#     low = c(NA, conf.low),
+#     high = c(NA, conf.high)
+# )
+
+dat <- data_age |> select(-label) |> slice((n_cat-3):n_cat) |> 
+  transmute(
+    group = LETTERS[1:(n_cat-1)] |>
+      ordered(sort(LETTERS[1:4], decreasing = TRUE)),
+    cen = estimate,
+    low = conf.low,
+    high = conf.high
   )
 
-# level label
+
+plot_1 <- ggplot(dat,aes(cen,group)) + 
+  geom_point(size=5, shape=18) +
+  geom_errorbarh(aes(xmax = high, xmin = low), height = 0.15) +
+  geom_vline(xintercept = 0, linetype = "longdash") +
+  scale_x_continuous(breaks = seq(0,14,1), labels = seq(0,14,1)) +
+  labs(x="Beta", y="")
+
+# merge
+data_tab <- ggplot(lab, aes(x = cols_table, 
+                  y = rows_table, 
+                  label = format(names_cols, nsmall = 1))) +
+  geom_text(size = 4, hjust=0, vjust=0.5) + 
+  theme_bw() +
+  # geom_hline(aes(yintercept=c(6.5,7.5))) + 
+  theme(panel.grid.major = element_blank(), 
+        legend.position = "none",
+        panel.border = element_blank(), 
+        axis.text.x = element_text(colour="white"),#element_blank(),
+        axis.text.y = element_blank(), 
+        axis.ticks = element_line(colour="white"),#element_blank(),
+        plot.margin = unit(c(0,0,0,0), "lines")) +
+  labs(x="",y="") +
+  coord_cartesian(xlim=c(1,4.5))
+
+grid.arrange(data_tab, plot_1, ncol=2)
