5ba7c6bb7e1b8ce9a19963a0944fec4bd0ae5d29
Jesus <jesus.sierralaya@hotmail.com>
2022-12-19 00:14:27 +0100

    Automatitation reference and indexation characters for labels in first row

--- notebooks/multi_table_forest_plot_v2.R
+++ notebooks/multi_table_forest_plot_v2.R
@@ -14,8 +14,8 @@ rm(list=setdiff(ls(), "tbl_multi"))
 
 # DATA
 tbl_multi_values <- 
-  tbl_multi$table_body |> 
-  select(variable, label, estimate, starts_with("conf")) 
+  tbl_multi$table_body# |> 
+  # select(variable, label, estimate, starts_with("conf")) 
 
 #EXAMPLES AND USEFUL CODE -----------------------------
 # Example add coordinates for plot (easy way)
@@ -55,9 +55,9 @@ tbl_multi_values <-
 # BUILD VECTOR CONTENT -------------------------------
 
 # First col: label (variables name, values names, refs.)
-col_1 <- 
-  tbl_multi_values |> 
-  select(label) |> pull() |> as.character()
+# col_1 <- 
+#   tbl_multi_values |> 
+#   select(label) |> pull() |> as.character()
 
 # TRY AUTOMATITATION
 # Format 
@@ -66,32 +66,45 @@ col_1 <-
 
 # Manual
 indexation <- "    "
-# Age
-col_1[2] <- paste0(indexation, "ref(",col_1[2],")")
-col_1[3] <- paste(indexation, col_1[3])
-col_1[4] <- paste(indexation, col_1[4])
-col_1[5] <- paste(indexation, col_1[5])
-# Sex
-col_1[7] <- paste0(indexation, "ref(",col_1[7],")")
-col_1[8] <- paste(indexation, col_1[8])
-# Ed
-col_1[10] <- paste0(indexation, "ref(",col_1[10],")")
-col_1[11] <- paste(indexation, col_1[11])
-col_1[12] <- paste(indexation, col_1[12])
-col_1[13] <- paste(indexation, col_1[13])
-# Marital
-col_1[15] <- paste0(indexation, "ref(",col_1[15],")")
-col_1[16] <- paste(indexation, col_1[16])
-col_1[17] <- paste(indexation, col_1[17])
-# Virtual
-col_1[19] <- paste0(indexation, "ref(",col_1[19],")")
-col_1[20] <- paste(indexation, col_1[20])
-col_1[21] <- paste(indexation, col_1[21])
-col_1[22] <- paste(indexation, col_1[22])
-# Social changes
-col_1[24] <- paste0(indexation, "ref(",col_1[24],")")
-col_1[25] <- paste(indexation, col_1[25])
-col_1[26] <- paste(indexation, col_1[26])
+# # Age
+# col_1[2] <- paste0(indexation, "ref(",col_1[2],")")
+# col_1[3] <- paste(indexation, col_1[3])
+# col_1[4] <- paste(indexation, col_1[4])
+# col_1[5] <- paste(indexation, col_1[5])
+# # Sex
+# col_1[7] <- paste0(indexation, "ref(",col_1[7],")")
+# col_1[8] <- paste(indexation, col_1[8])
+# # Ed
+# col_1[10] <- paste0(indexation, "ref(",col_1[10],")")
+# col_1[11] <- paste(indexation, col_1[11])
+# col_1[12] <- paste(indexation, col_1[12])
+# col_1[13] <- paste(indexation, col_1[13])
+# # Marital
+# col_1[15] <- paste0(indexation, "ref(",col_1[15],")")
+# col_1[16] <- paste(indexation, col_1[16])
+# col_1[17] <- paste(indexation, col_1[17])
+# # Virtual
+# col_1[19] <- paste0(indexation, "ref(",col_1[19],")")
+# col_1[20] <- paste(indexation, col_1[20])
+# col_1[21] <- paste(indexation, col_1[21])
+# col_1[22] <- paste(indexation, col_1[22])
+# # Social changes
+# col_1[24] <- paste0(indexation, "ref(",col_1[24],")")
+# col_1[25] <- paste(indexation, col_1[25])
+# col_1[26] <- paste(indexation, col_1[26])
+
+# Change labels
+col_1 <- tbl_multi_values |> 
+  select(reference_row, label, row_type) |> 
+  mutate(
+    label_new = case_when(
+      reference_row == TRUE ~ paste0(indexation, "Ref(",label, ")"),
+      reference_row == FALSE & row_type == "level" ~ paste0(indexation, label),
+      TRUE ~ label
+    ) 
+  ) |> 
+  select(label_new) |> pull() |> as.character()
+
 
 # Second col: beta
 col_2 <- tbl_multi_values |> 
@@ -99,15 +112,16 @@ col_2 <- tbl_multi_values |>
   mutate_all(as.character)|> 
   replace_na(list(estimate = "")) |> pull()
 
-# Third col:
+# Third col: ci (together)
+# 5 col q-value
+# col aGVIF
 
-# 
-n_cols <- 2
 
 # NAME COLS (FIRST ROW NAMES) ----------------------
-
 names_col <- c("VARIABLES", "BETA")
 
+# Number of cols 
+n_cols <- ls(pattern = "col_") |> length()
 # Combine one vector
 # add names cols
 content_col <- c(names_col[1], col_1, 
@@ -186,5 +200,3 @@ forest_plot <- data_foresplot |>
         panel.border = element_blank(),
   ) &
   plot_layout(widths = c(2,1))  
-
-
